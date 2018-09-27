df0c64e354a812afa7419524d85b31534635b2ab
lizhi <zhi.li.wb@external.99bill.com>
2018-09-27 10:45:15 +0800

    Modify project setting

---
+++ WorkersFreeTime/WorkersFreeTime/code/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  WorkersFreeTime
+//
+//  Created by 栗志 on 2018/9/27.
+//  Copyright © 2018年 com.lizhi1026. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
